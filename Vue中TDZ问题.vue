12bf3cc722727a6fee14d957d9515741f5e0c1d5
syh <xiaoxin@qq.com>
2021-12-18 15:26:39 +0800

    first commit

---
+++ Vue中TDZ问题.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <button type="" @click="test">sss</button>
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    window.test2 = this.test;
+  },
+  methods: {
+    test() {
+      "use strict";
+      console.log(scope);    //undefined
+      let scope = "local";
+      console.log(scope);    //"local"
+    },
+  },
+};
+</script>
+<style scoped></style>
